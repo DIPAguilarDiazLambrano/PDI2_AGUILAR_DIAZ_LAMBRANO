eb8923fb7a5ac04e8848eee3ada47eb13cdee927
Jelambrar96 <jelambrar@gmail.com>
2018-03-09 15:56:39 -0500

    commit10

--- problem_3.py
+++ problem_3.py
@@ -1,28 +1,30 @@
 import cv2 #libreria de procesamiento de imagenes
+import numpy as np
 from scipy import ndimage #liberia contiene filtros
 from matplotlib import pyplot as plt #libreria de graficos de imagenes
 #%matplotlib inline
-#------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+def blurre_image(image, sigma):
+    return ndimage.gaussian_filter(image, sigma = sigma)
+    
+#------------------------------------------------------------------------------    
 def main():
-    imagen = cv2.imread('test_pattern_blurring_orig.PNG',cv2.IMREAD_COLOR) 
+    imagen = cv2.imread('images/test_pattern_blurring_orig.png',cv2.IMREAD_COLOR) 
+    sigma_list = [3, 5, 9, 15, 35]
     #Carga la imagen con el formato RGB
-    plt.figure()
-    blurred = ndimage.gaussian_filter(imagen, sigma=3)
-    very_blurred = ndimage.gaussian_filter(imagen, sigma=5)
-    very_very_blurred = ndimage.gaussian_filter(imagen, sigma=9)
-    very_very_very_blurred = ndimage.gaussian_filter(imagen, sigma=15)
-    very_very_very_very_blurred = ndimage.gaussian_filter(imagen, sigma=35)
-    plt.imshow(imagen)
+    plt.figure(figsize = (14, 14))
+    plt.subplot(2, 3, 1)
+    plt.imshow(imagen, cmap='gray')
+    plt.axis('off')
+    plt.title('Original Image')
+    pos = 2     
+    for sigma in sigma_list:
+        plt.subplot(2, 3, pos)
+        pos = pos + 1
+        blurred_image = blurre_image(imagen, sigma)
+        plt.imshow(blurred_image, cmap='gray')
+        plt.axis('off')
+        plt.title('Blurred Image. sigma = ' + str(sigma))
     plt.show()
-    plt.imshow(blurred)
-    plt.show()
-    plt.imshow(very_blurred)
-    plt.show()
-    plt.imshow(very_very_blurred)
-    plt.show()
-    plt.imshow(very_very_very_blurred)
-    plt.show()
-    plt.imshow(very_very_very_very_blurred)
-    plt.show()
-#------------------------------------------------------------------------
+#----------------------------------------------------------------------------
 main()

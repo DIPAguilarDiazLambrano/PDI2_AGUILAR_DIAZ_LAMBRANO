476b05c74c0f7adf00617bc4aed86de692f9421b
Jelambrar96 <jelambrar@gmail.com>
2018-03-10 18:08:44 -0500

    commit14

--- problem_6.py
+++ problem_6.py
@@ -1,47 +1,66 @@
-from os import listdir, path
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
-
-
-def sobel_operator(image):
-    sobel_kernel_x = np.array([[-1, -2, -1],[0, 0, 0], [1, 2, 1]])
-    sobel_kernel_y = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-
+#%matplotlib inline
+#----------------------------------------------------------------------------
+#global variables
+sobel_kernel_x = np.array([[-1, -2, -1],[0, 0, 0], [1, 2, 1]])
+sobel_kernel_y = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+#------------------------------------------------------------------------------
+def gradient(image):
     gradient_x = cv2.filter2D(image,-1,sobel_kernel_x)
     gradient_y = cv2.filter2D(image,-1,sobel_kernel_y)
+    return [gradient_x, gradient_y]
+#------------------------------------------------------------------------------
+def magnitude_gradient(image):
+    gradient_x, gradient_y = gradient(image)
+    mag_grad =  np.power(np.power(gradient_x,2) + np.power(gradient_y,2),0.5)
+    return mag_grad
+#------------------------------------------------------------------------------
+def aprox_gradient(image):
+    gradient_x, gradient_y = gradient(image)
+    ap_gradient = np.absolute(gradient_x) + np.absolute(gradient_y)    
+    return ap_gradient
+#------------------------------------------------------------------------------
+def plot_image(img, title):
+    plt.imshow(img,cmap = 'gray')
+    plt.title(title)
+    plt.axis('off')
+#------------------------------------------------------------------------------
+def processImg(name):
+    # COMPUTER 
+    # original image      
+    img = cv2.imread("images/"+name, cv2.IMREAD_GRAYSCALE)
+    # gradient_x and y
+    [Gx, Gy] = gradient(img)
+    # magnitude_grad
+    mag_grad = magnitude_gradient(img)
+    # aprox_gradient
+    ap_gradient = aprox_gradient(img)
 
-    magnitude_grad =  np.power(np.power(gradient_x,2) + np.power(gradient_y,2),0.5)
+    # display original image 
+    plt.figure(figsize=(12,12))
+    plt.subplot(1,2,1)
+    plot_image(img, 'Original Image')
     
-    aprox_gradient = np.absolute(gradient_x) + np.absolute(gradient_y)
-
-    return [magnitude_grad,aprox_gradient,gradient_x,gradient_y]
-
+    # display gradients
+    plt.figure(figsize=(12,12))
+    plt.subplot(2,2,1)
+    plot_image(Gx, 'Gradient X')
+    plt.subplot(2,2,2)
+    plot_image(Gy, 'Gradient Y')
+    # display gradient magnitude 
+    plt.subplot(2,2,3)
+    plot_image(mag_grad, 'Magnitude Gradient')
+    plt.subplot(2,2,4)
+    plot_image(ap_gradient, 'Magnitude Gradient Aproximation')
+    
+    plt.suptitle('Gradient', fontsize=16)
+#------------------------------------------------------------------------------
 def main():
-    filenames = listdir('images')
-
-    for i,name in enumerate(filenames):
-        print(name)
-        img = cv2.imread("images/"+name, cv2.IMREAD_GRAYSCALE)
-        fig = plt.figure(figsize=(14,14));
-        plt.subplot(3,2,1)
-        plt.imshow(img,cmap='gray'), plt.title("Original")
-       
-        [magnitude_grad,aprox_gradient,gx,gy]=sobel_operator(img)
-        
-        plt.subplot(3,2,3),plt.imshow(gx,cmap = 'gray')
-        plt.title('Gradient X')
-        
-        plt.subplot(3,2,4),plt.imshow(gy,cmap = 'gray')
-        plt.title('Gradient Y')
-        
-        plt.subplot(3,2,5),plt.imshow(aprox_gradient,cmap = 'gray')
-        plt.title('Gradient magnitude aproximation')
-        
-        plt.subplot(3,2,6),plt.imshow(magnitude_grad,cmap= 'gray')
-        plt.title('Gradient magnitude')
-        
-        plt.show()
-
+    filenames = ['contact_lens_original.png', 'face.png']
+    for name in filenames:
+        processImg(name)
+    plt.show()
+#------------------------------------------------------------------------------
 main()

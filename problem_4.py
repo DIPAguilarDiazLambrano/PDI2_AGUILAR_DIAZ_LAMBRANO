d71d62171ddbd745ed1cdad10a92cbbfc3b2d273
Jelambrar96 <jelambrar@gmail.com>
2018-03-09 20:49:37 -0500

    commit11

--- problem_4.py
+++ problem_4.py
@@ -1,29 +1,31 @@
 import cv2
 import matplotlib.pyplot as plt
 %matplotlib inline
-
+#------------------------------------------------------------------------------
 def median_filter(image,n):
     return cv2.medianBlur(image,n)
-
+#------------------------------------------------------------------------------
 def averaging_filter(image,n):
     return cv2.blur(image,(n,n))
-
-imagen = cv2.imread("images/ckt_board_saltpep_prob_pt05.png", cv2.IMREAD_GRAYSCALE)
-plt.figure()
-plt.imshow(imagen, cmap='gray')
-plt.title('Imagen con Salt-Pepper noise')
-plt.show()
-
-kernel_size = [3,5,9,15] 
-for n in kernel_size:
-    plt.figure()
-    
+#------------------------------------------------------------------------------
+def main():
+    imagen = cv2.imread("images/ckt_board_saltpep_prob_pt05.png", cv2.IMREAD_GRAYSCALE)
+    plt.figure(figsize = (12,12))
     plt.subplot(1,2,1)
-    plt.imshow(median_filter(imagen,n), cmap='gray')
-    plt.title('Median filter n='+str(n))
-    
-    plt.subplot(1,2,2)
-    plt.imshow(averaging_filter(imagen,n), cmap='gray')
-    plt.title('Averaging filter n='+str(n))
-    
+    plt.imshow(imagen, cmap='gray')
+    plt.axis('off')
+    plt.title('Image with Salt-Pepper noise')
+    kernel_size = [3, 5, 9, 15]
+    for n in kernel_size:
+        plt.figure(figsize = (12,12))
+        plt.subplot(1,2,1)
+        plt.imshow(median_filter(imagen,n), cmap='gray')
+        plt.title('Median filter n='+str(n))
+        plt.axis('off')
+        plt.subplot(1,2,2)
+        plt.imshow(averaging_filter(imagen,n), cmap='gray')
+        plt.title('Averaging filter n='+str(n))
+        plt.axis('off')
     plt.show()
+#-------------------------------------------------------------------------------
+main()
---
+++ problem_4b.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+#-------------------------------------------------------------------------------
+def salt_peeper_noise(imagen,porcentaje):
+    imagen_out = imagen.copy()
+    porcentaje = porcentaje/100.0
+    npuntos = int(porcentaje/2 * imagen.size) #numero de puntos de sal y pimienta
+    salt_points = [np.random.randint(0, i - 1, npuntos) for i in imagen.shape]
+    imagen_out[salt_points] = 255 #Salt
+    peeper_points = [np.random.randint(0, i - 1, npuntos) for i in imagen.shape]
+    imagen_out[peeper_points] = 0 #Pepper
+    return imagen_out
+#-------------------------------------------------------------------------------
+def median_filter(image,n):
+    return cv2.medianBlur(image,n)
+#-------------------------------------------------------------------------------
+def averaging_filter(image,n):
+    return cv2.blur(image,(n,n))
+#-------------------------------------------------------------------------------
+def processImage(original_image):
+    fig = plt.figure(figsize=(12,12))
+    
+    plt.subplot(2,2,1)
+    plt.imshow(original_image, cmap='gray')
+    plt.title('Imagen Original')
+    plt.axis('off')
+    noise_image = salt_peeper_noise(original_image,25)
+    
+    plt.subplot(2,2,2)
+    plt.imshow(noise_image, cmap='gray')
+    plt.title('Imagen con ruido')
+    plt.axis('off')
+
+    plt.subplot(2,2,3)
+    plt.imshow(median_filter(noise_image,3), cmap='gray')
+    plt.title('Median filter n = 3')
+    plt.axis('off')
+
+    plt.subplot(2,2,4)
+    plt.imshow(median_filter(noise_image,5), cmap='gray')
+    plt.title('Median filter n=5')
+    plt.axis('off')
+
+    fig.tight_layout() 
+#-------------------------------------------------------------------------------
+def main():
+    imagenes = ["test_pattern_blurring_orig.png","pollen.jpg"]
+
+    for name in imagenes:
+        original_image = cv2.imread("images/" + name, cv2.IMREAD_GRAYSCALE)
+        processImage(original_image)
+        
+    plt.show()
+#--------------------------------------------------------------------------------------
+main()
